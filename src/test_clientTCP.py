61b894e1b4df51189433be7c69fcb79bb9c69ad2
joalopez1206 <joalopez@ug.uchile.cl>
2023-10-10 18:02:04 -0300

    added tests for stop and wait

---
+++ src/test_clientTCP.py
@@ -0,0 +1,14 @@
+from socketTCP import SocketTCP
+from utilsUDP import SV_ADDR
+# CLIENT
+client_socketTCP = SocketTCP()
+client_socketTCP.connect(SV_ADDR)
+# test 1
+message = "Mensje de len=16".encode()
+client_socketTCP.send(message)
+# test 2
+message = "Mensaje de largo 19".encode()
+client_socketTCP.send(message)
+# test 3
+message = "Mensaje de largo 19".encode()
+client_socketTCP.send(message)
