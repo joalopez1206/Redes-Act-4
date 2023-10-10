309f3d63ed07386bba977d205f09f6821f034ae4
joalopez1206 <joalopez@ug.uchile.cl>
2023-10-10 18:02:22 -0300

    fix: bug with send_full_msg

--- src/utilsUDP.py
+++ src/utilsUDP.py
@@ -1,10 +1,11 @@
 import socket
 from socketTCP import LEN_HEADERS, SocketTCP, DATA, SYN, ACK, FIN, SEQ
+
 SV_ADDR = ("localhost", 8000)
 BUFFER_SIZE = 16
 
 
-def recvfrom_full_msg(sock: socket.socket, buffsize=LEN_HEADERS+BUFFER_SIZE):
+def recvfrom_full_msg(sock: socket.socket, buffsize=LEN_HEADERS + BUFFER_SIZE):
     while True:
         msg, addr = sock.recvfrom(buffsize)
         msg = SocketTCP.parse_segment(msg.decode())[DATA]
@@ -15,16 +16,13 @@ def send_full_msg(sock: socket.socket, msg: bytes, addr, buffsize=BUFFER_SIZE):
     cursor = 0
     len_msg = len(msg)
 
-    len_of_msg_2_send = min(cursor + buffsize, len_msg)
-    msg2send = msg[cursor: cursor + buffsize].decode()
-    data_w_h = {SYN: '0', ACK: '0', FIN: '0', SEQ: '50', DATA: msg2send}
-    sock.sendto(SocketTCP.create_segment(data_w_h).encode(), addr)
-    cursor += buffsize
-    while len_of_msg_2_send < len_msg:
-
-        len_of_msg_2_send = min(cursor + buffsize, len_msg)
-        msg2send = msg[cursor: cursor + buffsize].decode()
+    while True:
+        len_of_msg_2_send = min(buffsize, len_msg - cursor)
+        msg2send = msg[cursor: cursor + len_of_msg_2_send].decode()
+        assert len_of_msg_2_send == len(msg2send), f"{len_of_msg_2_send} vs {len(msg2send)}"
         data_w_h = {SYN: '0', ACK: '0', FIN: '0', SEQ: '50', DATA: msg2send}
         sock.sendto(SocketTCP.create_segment(data_w_h).encode(), addr)
-        cursor += buffsize
+        cursor += len_of_msg_2_send
+        if cursor >= len_msg:
+            break
     return
